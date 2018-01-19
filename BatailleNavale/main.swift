5b8aafa43fe7dda755bf2171ceaa3396d35431d7
Benoit NEUTS <beneuts@iutclinfa1009m.local>
2018-01-19 08:05:17 +0100

    Initial Commit

---
+++ BatailleNavale/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BatailleNavale
+//
+//  Created by Benoit NEUTS on 19/01/2018.
+//  Copyright © 2018 Benoit NEUTS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
